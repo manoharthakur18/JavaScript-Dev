28749de20aa22825dc67cf236caa069e00a86da5
Manohar Thakur <manoharmkt.182@gmail.com>
2023-10-31 10:56:04 +0530

    Single post component

--- blogapp/src/components/SinglePost.jsx
+++ blogapp/src/components/SinglePost.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import "./singlepost.css";
+import { Link } from "react-router-dom";
 
 function SinglePost() {
   return (
-    <div>SinglePost</div>
-  )
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor
+          <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span>
+            Author:
+            <b className="singlePostAuthor">
+              <Link className="link" to="/posts?username=Safak">
+                Safak
+              </Link>
+            </b>
+          </span>
+          <span>1 day ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos!
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default SinglePost
+export default SinglePost;
