cfb5f5edfee532ee03e264320b3103fa99ecfbd7
Manohar Thakur <manoharmkt.182@gmail.com>
2023-11-01 11:35:44 +0530

    New comment section added

--- blogapp/src/components/CommentForm.jsx
+++ blogapp/src/components/CommentForm.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CommentForm() {
-  return <div>CommentForm</div>;
+function CommentForm({ handleSubmit, submitLabel }) {
+  const [text, setText] = useState("");
+
+  const isTextAreaDisabled = text.length === 0;
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    handleSubmit(text);
+    setText("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <textarea
+        className="comment-form-textarea"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+      />
+      <button className="comment-form-button" disabled={isTextAreaDisabled}>
+        {submitLabel}
+      </button>
+    </form>
+  );
 }
 
 export default CommentForm;
--- blogapp/src/components/Comments.jsx
+++ blogapp/src/components/Comments.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./comments.css";
-import { getComments as getCommentsApi } from "../Assets/CommentsApi";
+import {
+  getComments as getCommentsApi,
+  createComment as createCommentApi,
+} from "../Assets/CommentsApi";
 import Comment from "./Comment";
-
+import CommentForm from "./CommentForm";
 function Comments({ currentUserId }) {
   const [backendComments, setBackendComments] = useState([]);
   const rootComments = backendComments.filter(
@@ -24,15 +27,24 @@ function Comments({ currentUserId }) {
     });
   }, []);
 
+  const addComment = (text, parentId) => {
+    console.log("add comment", text, parentId);
+    createCommentApi(text, parentId).then((comment) => {
+      setBackendComments([comment, ...backendComments]);
+    });
+  };
+
   return (
     <div className="comments">
       <h3 className="comments-title">Comments</h3>
+      <div className="comment-form-title">Write Comment</div>
+      <CommentForm submitLabel="write" handleSubmit={addComment} />
       <div className="comments-container">
-        {rootComments.map((rootcomment) => (
+        {rootComments.map((rootComment) => (
           <Comment
-            key={rootcomment.id}
-            comment={rootcomment}
-            replies={getReplies(rootcomment.userId)}
+            key={rootComment.id}
+            comment={rootComment}
+            replies={getReplies(rootComment.id)}
           />
         ))}
       </div>
