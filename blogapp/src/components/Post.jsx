0e37d4538f7fad1fa0afdaa4163d3b853435aad8
Manohar Thakur <manoharmkt.182@gmail.com>
2023-11-01 18:50:05 +0530

    added external file and modified post component and post screen for post data for viewing dynamic data on screen

--- blogapp/src/components/Post.jsx
+++ blogapp/src/components/Post.jsx
@@ -2,37 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./post.css";
 
-function Post({ img }) {
+function Post({ post }) {
   return (
     <div className="post">
-      <img className="postImg" src={img} alt="" />
+      <img className="postImg" src={post.img} alt="" />
       <div className="postInfo">
         <div className="postCats">
           <span className="postCat">
             <Link className="link" to="/posts?cat=Music">
-              Music
-            </Link>
-          </span>
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Life
+              {post.category}
             </Link>
           </span>
         </div>
         <span className="postTitle">
           <Link to="/post/abc" className="link">
-            Lorem ipsum dolor sit amet
+            {post.title}
           </Link>
         </span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">{post.date}</span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-        atque, exercitationem quibusdam, reiciendis odio laboriosam?
-      </p>
+      <p className="postDesc">{post.description}</p>
     </div>
   );
 }
