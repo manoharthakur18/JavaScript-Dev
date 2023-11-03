3f676e023605763ad1f93c1e5e68c7a69acd1be0
Manohar Thakur <manoharmkt.182@gmail.com>
2023-11-03 12:37:53 +0530

    moudles & anonymous functions

--- NodeBasics/server.js
+++ NodeBasics/server.js
@@ -1,9 +1,21 @@
 var http = require("http");
+var module1 = require("./module1");
+var module2 = require("./module2");
 
-function onRequest(request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello World!");
-  response.end();
-}
+// function onRequest(request, response) {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.write(module2.myVariable);
+//   module2.myFunction();
+//   response.end();
+// }
 
-http.createServer(onRequest).listen(5000);
+// http.createServer(onRequest).listen(5000);
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write(module2.myVariable);
+    module2.myFunction();
+    response.end();
+  })
+  .listen(5000);
