2a790d091088983f32d4d5887dff1fa930aa66db
Manohar Thakur <manoharmkt.182@gmail.com>
2023-11-06 15:23:40 +0530

    user registration completed

--- blogapi/index.js
+++ blogapi/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongooes = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongooes
   .connect(process.env.MONGO_URL, {
@@ -14,6 +16,8 @@ mongooes
     console.log("Connected to mongo");
   })
   .catch((err) => console.log("Error ", err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Backed is running");
