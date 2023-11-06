950e60ed905a7d31f6c31e68f64b3b04e3942157
Manohar Thakur <manoharmkt.182@gmail.com>
2023-11-06 14:37:35 +0530

    Schema added

---
+++ blogapi/models/Category.js
@@ -0,0 +1,13 @@
+const mongooes = require("mongoose");
+
+const CategorySchema = new mongooes.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongooes.model("Post", CategorySchema);
---
+++ blogapi/models/Post.js
@@ -0,0 +1,30 @@
+const mongooes = require("mongoose");
+
+const PostSchema = new mongooes.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongooes.model("Post", PostSchema);
