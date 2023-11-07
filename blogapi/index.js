7f75563a89d1f49fff365d655d950afa0f3495bd
Manohar Thakur <manoharmkt.182@gmail.com>
2023-11-07 15:06:56 +0530

    user crud completed

--- blogapi/index.js
+++ blogapi/index.js
@@ -3,21 +3,21 @@ const app = express();
 const dotenv = require("dotenv");
 const mongooes = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
 
 mongooes
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connected to mongo");
   })
   .catch((err) => console.log("Error ", err));
 
 app.use("/api/auth", authRoute);
+
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
   console.log("Backed is running");
